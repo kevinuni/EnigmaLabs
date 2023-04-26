11711a154e80ae01cd4000a282ff72a30c533a7e
kevin diaz <kevin@diaz.pe>
2023-04-26 00:21:02 -0500

    ApiClient

---
+++ ApiClient/PlanillaClient.cs
@@ -0,0 +1,24 @@
+﻿using Enigma.Domain.Dto;
+using Enigma.Util;
+
+namespace ApiClient;
+
+public class PlanillaClient
+{
+    HttpManager client;
+    private const string endpoint = "planilla";
+
+    public PlanillaClient()
+    {
+        if (client == null)
+        {
+            client = HttpManager.Instance();
+            client.InitializeClient("http://localhost:5000/api");
+        }
+    }
+    public async Task<PersonaDto> Get(string periodo)
+    {
+        var result = await client.Get<PersonaDto>(endpoint + "/" + periodo);
+        return result.Data;
+    }
+}
